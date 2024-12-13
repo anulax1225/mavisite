c4f0015c5bd563bb52b4f849a1cc964effa35faf
anulax1225 <anulax1225@icloud.com>
2024-12-13 23:49:56 +0100

    Ajout d'un jolie footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,45 @@
 @extends("layout")
 
 @section("content")
-    <div class="relative flex flex-col items-center justify-center overflow-hidden mt-12">
-        <img class="w-full absolute right-0 left-0 -z-10" src="/img/immo.jpg">
+    <div class="relative flex flex-col items-center justify-center overflow-hidden">
+        <img class="w-full absolute right-0 left-0 -z-10 blur-[3px]" src="/img/immo.jpg">
         <div class="bg-gradient-to-tr from-orange-600 to-orange-400 rounded-lg shadow-md shadow-gray-800 flex flex-col items-center p-10 mt-10 mb-10 w-fit">
-            <h5 class="text-[6rem] font-bold mx-auto leading-none">MaVisite360°</h5>
+            <h5 class="text-[6rem] font-bold mx-auto leading-none">Ma Visite 360</h5>
             <h5 class="text-[1.2rem] italic text-center mx-auto">Ma visite, mon choix à 360°</h5>
         </div>
     </div>
     <div class="flex flex-col w-3/4 h-fit mx-auto mb-10">
         <h1 class="mt-10 px-3 text-[3rem] font-bold">Histoire</h1>
-        <h2 class="px-3 mx-auto text-[1.4rem] font-semibold">Fondée en 2024 et basée à Neuchâtel. MaVisite360 est une entreprise fictive spécialisée dans les technologies immersives, avec un site web accessible sous le nom de domaine mv360.ch.</h2>
-        <h2 class="mt-5 px-3 mx-auto text-[1.1rem]">MaVisite360 propose un service novateur de visites virtuelles immersives 360°, destiné principalement aux agences immobilières. Notre solution clé en main englobe toutes les étapes : prise de contact, capture de photos professionnelles, création de visites virtuelles interactives et hébergement en ligne. Cette approche permet une intégration simple et rapide sur les sites web des clients.</h2>
+        <h2 class="px-3 mx-auto text-[1.4rem] font-semibold">
+            Fondée en 2024 et basée à Neuchâtel. 
+            "Ma Visite 360" est une entreprise fictive spécialisée dans les technologies immersives, 
+            avec un site web accessible sous le nom de domaine mv360.ch.
+        </h2>
+        <h2 class="mt-5 px-3 text-[1.3rem]">
+            "Ma Visite 360" propose un service novateur de visites virtuelles immersives 360°, 
+            destiné principalement aux agences immobilières.<br> 
+            Notre solution clé en main englobe toutes les étapes : 
+        </h2>
+        <ul class="text-gray-50 text-[1.3rem] list-disc px-10">
+            <li><h3 class="font-semibold">Prise de contact.</h3></li>
+            <li><h3 class="font-semibold">Capture de photos professionnelles.</h3></li>
+            <li><h3 class="font-semibold">Création de visites virtuelles interactives et hébergement en ligne.</h3></li>
+        </ul>
+        <h2 class="px-3 text-[1.3rem] mt-1">
+            Cette approche permet une intégration simple et rapide sur les sites web des clients.
+        </h2>
+        
         <div class="w-full h-fit flex items-center justify-center flex-col">
             <div class="mt-5 flex w-full items-center">
-                <div class="w-[40%] p-3 m-3 bg-gradient-to-tr from-orange-600 to-orange-500 shadow-md shadow-gray-900 rounded-lg">
+                <div class="w-[40%] p-3 hover:scale-105 m-3 bg-gradient-to-tr from-orange-600 to-orange-500 shadow-md shadow-gray-900 rounded-lg">
                     <h5 class="text-[2rem] font-semibold w-full text-center">Vision</h5>
                     <h5 class="px-2 text-[1.2rem] font-semibold">Transformer la présentation immobilière en une expérience immersive, accessible et sans contraintes.</h5>
                 </div>
-                <div class="w-[40%] p-3 m-3 bg-gradient-to-tl from-orange-500 to-orange-600 shadow-md shadow-gray-900 rounded-lg">
+                <div class="w-[40%] p-6 hover:scale-105 m-3 bg-gradient-to-tl from-orange-500 to-orange-600 shadow-md shadow-gray-900 rounded-lg">
                     <h5 class="text-[2rem] font-semibold text-center">Mission</h5>
                     <h5 class="px-2 text-[1.2rem] pb-2 font-semibold">Offrir une solution clé en main de visites virtuelles 360°, permettant aux agences immobilières et autres secteurs de valoriser leurs espaces et de gagner en efficacité.</h5>
                 </div>
-                <div class="w-[20%] p-3 m-3 bg-gradient-to-r from-orange-500 to-orange-600 shadow-md shadow-gray-900 rounded-lg">
+                <div class="w-[20%] p-3 hover:scale-105 m-3 bg-gradient-to-r from-orange-500 to-orange-600 shadow-md shadow-gray-900 rounded-lg">
                     <h5 class="text-[2rem] font-semibold text-center">Valeurs</h5>
                     <ul class="text-gray-50 text-[1.2rem] list-disc px-5">
                         <li><h5 class="font-semibold">Innovation</h5></li>
@@ -35,7 +52,7 @@
         
         <h1 class="px-3 text-[3rem] font-semibold mt-7">Services et offres</h1>
         <div class="w-full px-3 flex justify-between items-center">
-            <div class="w-[40%] p-5 mr-2 bg-gradient-to-r from-orange-500 to-orange-600 shadow-md shadow-gray-900 rounded-lg">
+            <div class="w-[40%] p-5 mr-2 hover:scale-[1.01] bg-gradient-to-r from-orange-500 to-orange-600 shadow-md shadow-gray-900 rounded-lg">
                 <h5 class="text-[2rem] font-semibold text-center">Visite</h5>
                 <ul class="text-gray-50 text-[1rem] list-disc px-5">
                     <li><h5 class="font-semibold mb-2">Capture des photos 360° haute définition.</h5></li>
@@ -43,7 +60,7 @@
                     <li><h5 class="font-semibold">Hébergement en ligne sur notre plateforme.</h5></li>
                 </ul>
             </div>
-            <div class="w-[60%] p-5 ml-1 bg-gradient-to-r from-orange-500 to-orange-600 shadow-md shadow-gray-900 rounded-lg">
+            <div class="w-[60%] p-8 ml-1 hover:scale-[1.01] bg-gradient-to-r from-orange-500 to-orange-600 shadow-md shadow-gray-900 rounded-lg">
                 <h5 class="text-[2rem] font-semibold text-center">Valeur ajoutée</h5>
                 <ul class="text-gray-50 text-[1rem] list-disc px-5">
                     <li><h5 class="font-semibold mb-2">Simplifie les visites pour les acheteurs potentiels.</h5></li>
@@ -66,7 +83,8 @@
                             <li><h5 class="text-[1rem]">Hébergement (inclue des frais supplémentaire)</h5></li>
                         </ul>
                     </div>
-                    <button class="mt-5 self-end w-1/3 rounded-lg bg-orange-600 px-4 py-2 text-white font-bold mb-2">Consulter l'offre</button>
+                    <button class="mt-5 self-end w-1/3 rounded-lg hover:scale-105
+                    bg-orange-600 px-4 py-2 text-white font-bold mb-2">Consulter l'offre</button>
                 </div>
             </div>
             <div class="w-1/2 p-[2px] m-2 bg-gradient-to-r from-orange-400 to-orange-600 shadow-md shadow-gray-900 rounded-lg">
@@ -81,7 +99,8 @@
                             <li><h5 class="text-[1rem]">Hébergement offert</h5></li>
                         </ul>
                     </div>
-                    <button class="mt-5 self-end w-1/3 rounded-lg bg-orange-600 px-4 py-2 text-white font-bold mb-2 hover:ring-1">Consulter l'offre</button>
+                    <button class="mt-5 self-end w-1/3 rounded-lg hover:scale-105
+                    bg-orange-600 px-4 py-2 text-white font-bold mb-2">Consulter l'offre</button>
                 </div>
             </div>
         </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,48 +9,93 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-        <title>Viewer 360 - {{ $title }}</title>
+        <title>Ma Visite 360 - {{ $title }}</title>
 
         @vite("resources/js/app.js")
         @vite("resources/css/app.css")
     </head>
-    <body class="relative w-screen h-screen antialiased font-robo bg-gray-850 -z-10">
+    <body class="w-screen h-screen antialiased font-robo bg-gray-850 -z-10">
         <!--<img src="/img/lightsource.png" class="w-full absolute top-0 right-0 left-0 -z-10">-->
-        <nav class="fixed right-0 left-0 top-0 h-14 z-50">
-            <div class="w-full h-full flex items-center justify-center bg-gradient-to-r from-gray-850 via-gray-750 to-gray-850 pb-[2px]">
+        <nav class="sticky right-0 left-0 top-0 h-16 z-50">
+            <div class="w-full h-full flex items-center justify-center bg-gradient-to-r from-gray-850 via-gray-600 to-gray-850 pb-[2px]">
                 <div class="w-full h-full px-[12.5%] flex justify-between bg-gray-800">
                     <div class="flex items-center h-full">
-                        <a href="/" class="h-full"><img src="/img/logo-mavisite360.png" class="h-full mr-2 p-2"></a>
-                        <p class="text-lg text-white bg-orange-500/90 rounded-lg px-3 font-semibold">{{ $title }}</p>
+                        <a href="/" class="h-full"><img src="/img/logo-mavisite360.png" class="h-full hover:scale-105 mr-2 p-3"></a>
+                        <p class="text-lg text-white bg-gradient-to-tr from-orange-400 to-orange-600 rounded-lg px-4 font-semibold">{{ $title }}</p>
                     </div>
                     <div class="flex items-center h-full">
-                        <a href="/"><h1 class="font-semibold mr-4 text-lg">home</h1></a>
-                        <a href="/demo"><h1 class="font-semibold mr-4 text-lg">demo</h1></a>
-                        <h1 class="font-semibold mr-9 text-lg">contact</h1>
-                        <div class="flex items-center h-fit py-1 px-2 border-l border-gray-600">
-                            <button class="relative group flex items-center justify-center mr-6">
-                                <img src="/img/dark.svg" class="w-[1.2rem] dark:invert">
-                                <div class="absolute group-focus:block hidden bg-gray-800 -bottom-[100px] left-0 w-[160px] rounded-xl overflow-hidden">
+                        <a href="/"><h1 class="mr-4 text-xl font-bold hover:scale-105">home</h1></a>
+                        <a href="/demo"><h1 class="font-bold mr-4 text-xl hover:scale-105">demo</h1></a>
+                        <a href="#contact"><h1 class="font-bold mr-9 text-xl hover:scale-105">contact</h1></a>
+                        <div class="flex items-center h-fit py-[8px] pl-5 border-l border-gray-700">
+                            <button class="relative group flex items-center justify-center mr-6" 
+                            onclick="document.querySelector('#theme-panel').classList.toggle('hidden')">
+                                <img src="/img/dark.svg" class="w-[1.4rem] hover:scale-105 invert">
+                                <div id="theme-panel" class="absolute hidden bg-gray-800 -bottom-[110px] right-4/5 
+                                w-[160px] rounded-xl overflow-hidden">
                                     <div class="px-4 pt-2 grid grid-cols-4 hover:bg-gray-600 items-center">
-                                        <img src="/img/dark.svg" class="w-[1.4rem] dark:invert pl-1">
+                                        <img src="/img/dark.svg" class="w-[1.4rem] invert pl-1">
                                         <h1 class="font-semibold text-[1.1rem] col-span-3 mb-1 text-left">Dark</h1>
                                     </div>
                                     <div class="px-4 pb-2 grid grid-cols-4 hover:bg-gray-600 items-center">
-                                        <img src="/img/light.svg" class="w-[1.4rem] dark:invert">
+                                        <img src="/img/light.svg" class="w-[1.4rem] invert">
                                         <h1 class="font-semibold text-[1.1rem] mt-1 col-span-3 text-left">Light</h1>
                                     </div>
                                 </div>
                             </button>
-                            <img src="/img/instagram.svg" class="w-[1.4rem] dark:invert mr-3">
-                            <img src="/img/twitter.svg" class="w-[1.4rem] dark:invert">
+                            <img src="/img/instagram.svg" class="w-[1.6rem] hover:scale-105 invert mr-3">
+                            <img src="/img/twitter.svg" class="w-[1.6rem] hover:scale-105 invert">
                         </div>
                     </div>
                 </div>
             </div>
         </nav>
         @yield("content")
-        <footer class="w-full h-14 px-[13%] bg-gray-800 flex items-center justify-between">
-            <p class="italic">&copy; 2024 Créer par l'équipe MaVisite360</p>
+        <footer class="w-full bg-gray-800">
+            <div class="w-3/4 px-3 mx-auto flex flex-col items-center justify-between">
+                <div class="w-full mt-10 flex flex-col items-center">
+                    <h2 class="font-semibold text-[1.4rem] mb-1">Nos partenaires</h2>
+                    <div class="relative w-full flex items-center h-20 mb-5 overflow-hidden">
+                        <img src="/img/panoee.png" class="w-32 slider" style="--delay: 0s;">
+                        <img src="/img/infomaniak.png" class="w-32 slider" style="--delay: 2s;">
+                        <img src="/img/cpne.jpg" class="w-32 slider" style="--delay: 4s;">
+                        <img src="/img/entreprise_immo_1.png" class="w-32 slider" style="--delay: 6s;">
+                        <img src="/img/entreprise_immo_2.jpeg" class="w-32 slider" style="--delay: 8s;">
+                        <img src="/img/entreprise_immo_3.png" class="w-32 slider" style="--delay: 10s;">
+                        <img src="/img/entreprise_immo_4.png" class="w-32 slider" style="--delay: 12s;">
+                        <img src="/img/entreprise_immo_5.webp" class="w-32 slider" style="--delay: 14s;">
+                        <img src="/img/entreprise_immo_6.png" class="w-32 slider" style="--delay: 16s;">
+                    </div>
+                </div>
+                <div class="w-full mt-5 flex items-center">
+                    <div id="contact" class="w-1/3 mb-10">
+                        <h2 class="font-semibold text-[1.7rem] border-b-2 border-gray-750 w-3/4 pr-20 mb-2">Nous contacter</h2>
+                        <div class="px-3">
+                            <h4 class="font-semibold mb-1">+41 78 694 09 79</h4>
+                            <h4 class="font-semibold mb-1">contact@mv360.ch</h4>
+                            <h4 class="font-semibold">Rue de la Maladière 80</h4>
+                        </div>
+                    </div>
+                    <div class="flex items-start justify-evenly w-1/3 mb-10">
+                        <div class="hover:social-icon-in social-icon-out p-2 border border-gray-750 rounded-lg">
+                            <img src="/img/linkedin.svg" class="w-[2rem] brightness-0 invert">
+                        </div>
+                        <div class="hover:social-icon-in social-icon-out p-2 border border-gray-750 rounded-lg">
+                            <img src="/img/discord.svg" class="w-[2rem] invert">
+                        </div>
+                        <div class="hover:social-icon-in social-icon-out p-2 border border-gray-750 rounded-lg">
+                            <img src="/img/facebook.svg" class="w-[2rem] invert">
+                        </div>
+                        <div class="hover:social-icon-in social-icon-out p-2 border border-gray-750 rounded-lg">
+                            <img src="/img/instagram.svg" class="w-[2rem] invert">
+                        </div>
+                        <div class="hover:social-icon-in social-icon-out p-2 border border-gray-750 rounded-lg">
+                            <img src="/img/twitter.svg" class="w-[2rem] invert">
+                        </div>
+                    </div>
+                    <p class="italic w-1/3 text-right self-end">&copy; 2024 Créer par l'équipe Ma Visite 360</p>
+                </div>
+            </div>
         </footer>
     </body>
 </html>
